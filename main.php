e8d83874f825f5aad21e364a287fe3dcc1ae5103
mauthsalam <mauth4444@gmail.com>
2020-04-15 02:52:17 +0900

    first upload

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: SoccerPlayerSearch
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.mywebsite.com
+ */
